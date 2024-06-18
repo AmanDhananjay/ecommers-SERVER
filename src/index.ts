e13c68fc3a8dba9c15417bce0aa86d430dba6f9d
AmanDhananjay <112410719+AmanDhananjay@users.noreply.github.com>
2024-06-18 23:36:59 +0530

    first

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/user";
+import { productRouter } from "./routes/product";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+app.use("/products", productRouter);
+
+mongoose.connect(
+  "mongodb+srv://farzicopy08:aman2050@cluster0.ppkdrkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
+//mongodb+srv://amandhananjay:aman2050@cluster0.ojdiqld.mongodb.net/Cluster0
+//mongodb+srv://farzicopy08:<password>@cluster0.ppkdrkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+//mongodb+srv://farzicopy08:<password>@cluster0.ppkdrkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+app.listen(3001, () => console.log("Server started port 3001"));
